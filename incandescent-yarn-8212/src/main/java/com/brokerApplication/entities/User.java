95a30deca1919f6bd28bf37873c425f7d1e9f924
Kashish <kashishbansal044@gmail.com>
2023-03-28 22:51:53 +0530

    real estate day02 fw21_0546

---
+++ incandescent-yarn-8212/src/main/java/com/brokerApplication/entities/User.java
@@ -0,0 +1,26 @@
+package com.brokerApplication.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer userId;
+	private String username;
+	private String email;
+	private String mobile;
+	private String role;
+	private String city;
+	private String password;
+}
