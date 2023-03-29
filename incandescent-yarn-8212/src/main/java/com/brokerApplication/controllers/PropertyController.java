95145e400072c73361c7e0c2406fca763487ec0e
dikshant <dikshantdeogade72@gmail.com>
2023-03-29 15:52:01 +0530

    Day_2

---
+++ incandescent-yarn-8212/src/main/java/com/brokerApplication/controllers/PropertyController.java
@@ -0,0 +1,63 @@
+package com.brokerApplication.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.brokerApplication.entities.Property;
+import com.brokerApplication.exceptions.PropertyException;
+import com.brokerApplication.services.PropertyService;
+
+@RestController
+public class PropertyController {
+
+	@Autowired
+	private PropertyService propertyService;
+	
+	@PostMapping("/property")
+	public ResponseEntity<Property> addProperty(@RequestBody Property property ) 
+	{
+		Property savedProperty=propertyService.addProperty(property);
+		
+		return new ResponseEntity<Property>(savedProperty,HttpStatus.CREATED);
+		
+	}
+	
+	@PutMapping("/property")
+	public ResponseEntity<Property> editProperty(@RequestBody Property property) throws PropertyException{
+		
+		Property editedproperty=propertyService.editProperty(property);
+		return new ResponseEntity<Property>(editedproperty,HttpStatus.ACCEPTED);
+		
+	}
+	
+	@GetMapping("/property")
+	public ResponseEntity<List<Property>> ListAllPropertys() throws PropertyException{
+		List<Property> allPropertys=  propertyService.ListAllPropertys();
+		return new ResponseEntity<List<Property>>(allPropertys,HttpStatus.OK);
+	}
+	
+	@GetMapping("/property/{propertyId}")
+	public ResponseEntity<Property> viewPropertyById(@PathVariable("propertyId") Integer propertyId) throws PropertyException
+	{
+		Property property= propertyService.viewPropertyById(propertyId);
+		return new ResponseEntity<Property>(property,HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/property/{propertyId}")
+	public ResponseEntity<Property> removePropertyById(@PathVariable("propertyId") Integer propertyId) throws PropertyException
+	{
+		Property removedProperty= propertyService.removePropertyById(propertyId);
+		return new ResponseEntity<Property>(removedProperty,HttpStatus.ACCEPTED);
+	}
+	
+}
---
+++ incandescent-yarn-8212/src/main/java/com/brokerApplication/exceptions/PropertyException.java
@@ -0,0 +1,11 @@
+package com.brokerApplication.exceptions;
+
+public class PropertyException extends Exception{
+	
+	public PropertyException() {
+		
+	}
+    public PropertyException(String message) {
+		super(message);
+	}   
+}
